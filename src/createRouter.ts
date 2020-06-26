2649436af141a17f3366dd17266bd4537db91cce
diamond <53026600+diamondto@users.noreply.github.com>
2020-06-26 20:21:43 +0800

    Update createRouter.ts

--- src/createRouter.ts
+++ src/createRouter.ts
@@ -2,20 +2,20 @@
 import * as React from 'react';
 
 export interface RouterProviderProps {
-  route: string;
+  route: string;   
   fullRoute?: string;
   parent?: any;
 }
 
 const createRouter = () => {
   const context = React.createContext<RouterProviderProps>({
-    route: '',
+    route: '',   // 父子传旨
   });
 
   // not sure if this supposed to be unused, ignoring ts error for now
   // @ts-ignore
   const Router: React.SFC<RouterProviderProps> = props => {
-    const { route, fullRoute, parent, children } = props;
+    const { route, fullRoute, parent, children } = props;  // 解构出路由参数信息
 
     if (process.env.NODE_ENV !== 'production') {
       if (typeof route !== 'string') {
@@ -27,9 +27,9 @@ const createRouter = () => {
       value: {
         fullRoute: fullRoute || route,
         route,
-        parent,
+        parent,  // 与接口定义参数一致
       },
-      children,
+      children,  
     });
   };
 };
