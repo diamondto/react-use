02b64258756af0858181d51b0b70fc26e945d312
diamond <53026600+diamondto@users.noreply.github.com>
2020-06-26 20:17:37 +0800

    Update useEffectOnce.ts

--- src/useEffectOnce.ts
+++ src/useEffectOnce.ts
@@ -1,7 +1,7 @@
 import { EffectCallback, useEffect } from 'react';
 
 const useEffectOnce = (effect: EffectCallback) => {
-  useEffect(effect, []);
+  useEffect(effect, []);// 只调用一次，
 };
 
 export default useEffectOnce;
